3cca80b214d131a530f044092fbe768730432171
Ascyt <filip.schauer07@gmail.com>
2024-04-11 20:02:40 +0200

    Minor fixes and improvemetns

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { ThemeSwitcherService } from '../theme-switcher/theme-switcher.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [NgbModule],
+  imports: [NgbModule, CommonModule],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.scss'
 })
 export class ContactComponent {
-
+  public constructor(public themeSwitcherService:ThemeSwitcherService) {}
 }
